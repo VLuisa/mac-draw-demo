3a6510ed39ef3073e8991b4f87fcbb90cbf88f1a
pinkluna <luisavasquez@luisas-mbp.wireless.dartmouth.edu>
2017-09-14 11:06:14 -0400

    text editable by content, size, font, and color

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            circles: []
+            circles: [],
+            textCont: '',
+            textSize: '20',
+            fontType: 'Avenir',
+            color: '#ED6E46'
         };
 
         this.addCircle = this.addCircle.bind(this);
         this.renderCircles = this.renderCircles.bind(this);
+        this.renderText = this.renderText.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+        this.setTextSize = this.setTextSize.bind(this);
+        this.setFontType = this.setFontType.bind(this);
+        this.setColor = this.setColor.bind(this);
     }
 
     addCircle(e) {
@@ -22,6 +31,28 @@ class App extends Component {
         });
     }
 
+    renderText(t, size, font, color) {
+      return (
+          <text x="50" y="300" fill={color} fontSize={size} fontFamily={font}>{t}</text>
+      )
+    }
+
+    onInputChange(event) {
+      this.setState({textCont: event.target.value})
+    }
+
+    setTextSize(event) {
+      this.setState({textSize: event.target.value})
+    }
+
+    setFontType(event) {
+      this.setState({fontType: event.target.value})
+    }
+
+    setColor(event) {
+      this.setState({color: event.target.value})
+    }
+
     renderCircles() {
         console.log(this.state.circles);
         return this.state.circles.map(function(circle, i) {
@@ -30,11 +61,17 @@ class App extends Component {
             )
         });
     }
-    
+
     render() {
         return (
             <div className="App" onClick={this.addCircle}>
-                {this.renderCircles()}
+              <p>Content: <input onChange={this.onInputChange} /></p>
+              <p>Size: <input onChange={this.setTextSize} /></p>
+              <p>Font: <input onChange={this.setFontType} /></p>
+              <p>Color: <input onChange={this.setColor} /></p>
+              <svg width="1000" height="1000">
+                {this.renderText(this.state.textCont, this.state.textSize, this.state.fontType, this.state.color)}
+              </svg>
             </div>
         );
     }
